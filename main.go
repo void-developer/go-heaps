fac241510d3c8562a941918dd084fcbfaad1f38a
void-developer <angeli_leonardo@icloud.com>
2022-08-23 15:28:11 +0200

    [minor] Added documentation to the heap's structure and functions

--- main.go
+++ main.go
@@ -10,14 +10,20 @@ const (
 	MaxQ int = 0
 )
 
+// Heap is the base data structure for the binary heap. The heap has the basic tree and size properties.
+// Additional properties are:
+//	- Type: the type of the heap. Either MinQ or MaxQ.
+//	- Nil: the value that represents a null value in the heap.
+//
 type Heap[C types.Comparable] struct {
-	Tree    []C
-	Size    int
-	Type    int
-	Nil     C
-	Default C
+	Tree []C
+	Size int
+	Type int
+	Nil  C
 }
 
+// comparePriority uses the Compare method of the Heap type to compare the priorities of the two values.
+// It compares val1 to val2 if the heap is of type MaxQ, and it compares val2 to val1 if it's of type MinQ
 func (h *Heap[C]) comparePriority(val1 C, val2 C) int {
 	if h.Type == MaxQ {
 		return val1.Compare(val2)
@@ -28,10 +34,13 @@ func (h *Heap[C]) comparePriority(val1 C, val2 C) int {
 	}
 }
 
+// Peek returns the value of the top of the heap without removing it
 func (h *Heap[C]) Peek() C {
 	return h.Tree[0]
 }
 
+// Push pushes an element to the rightmost child of the last element of the binary tree and then rebalances the tree
+// using the siftup operation
 func (h *Heap[C]) Push(val C) {
 	h.Tree[h.Size] = val
 	h.Size += 1
@@ -40,6 +49,7 @@ func (h *Heap[C]) Push(val C) {
 	}
 }
 
+// siftUp moves the element at index ix up the tree until it's it's parent has a higher priority than it
 func (h *Heap[C]) siftUp() {
 	currentNode := h.Size - 1
 	for h.comparePriority(h.Tree[currentNode], h.Tree[heaps.GetParent(currentNode)]) > 0 {
@@ -51,6 +61,7 @@ func (h *Heap[C]) siftUp() {
 	}
 }
 
+// siftDown moves the element at index ix down the tree until its children have a lower priority than it
 func (h *Heap[C]) siftDown(ix int) {
 	currentNode := ix
 	children := heaps.GetChildren(currentNode)
@@ -76,6 +87,8 @@ func (h *Heap[C]) siftDown(ix int) {
 	}
 }
 
+// Replace replaces the value at the root of the heap, returns the old value and rebalances the tree performing
+// a siftdown operation
 func (h *Heap[C]) Replace(val C) C {
 	if !h.IsFull() {
 		h.Push(val)
@@ -87,6 +100,7 @@ func (h *Heap[C]) Replace(val C) C {
 	return popped
 }
 
+// Pop removes the top of the heap and rebalances the tree using the siftdown operation
 func (h *Heap[C]) Pop() C {
 	if h.Size == 0 {
 		return h.Nil
@@ -102,10 +116,12 @@ func (h *Heap[C]) Pop() C {
 	return popped
 }
 
+// IsFull returns true if the heap is full
 func (h *Heap[C]) IsFull() bool {
 	return h.Size == len(h.Tree)
 }
 
+// Init initializes the heap with the given type and nil value
 func (h *Heap[C]) Init(size int, _type int) {
 	h.Tree = make([]C, size)
 	for i := 0; i < size; i++ {
@@ -115,11 +131,13 @@ func (h *Heap[C]) Init(size int, _type int) {
 	h.Type = _type
 }
 
+// InitFromArray initializes the heap with the given type and nil value and then populates the heap with the given array
 func (h *Heap[C]) InitFromArray(arr []C) {
 	h.Tree = arr
 	h.Size = len(arr)
 }
 
+// Heapify creates a heap out of the given array. The heapification is an in-place operation
 func (h *Heap[C]) Heapify(arr []C) *Heap[C] {
 	h.Tree = arr
 	h.Size = len(arr)
